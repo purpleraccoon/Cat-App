2f74c999ae16771a58eb5392ce29d39d3e5e6237
Jeremy Kemery <jeremykemery@gmail.com>
2020-05-19 15:05:11 -0500

    Initial Commit

---
+++ Cat App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cat App
+//
+//  Created by Jeremy Kemery on 5/19/20.
+//  Copyright © 2020 Jeremy Kemery. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
